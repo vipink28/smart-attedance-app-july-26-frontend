bd9db7ebc184624c823d6d747d7720f6ab46ce9b
vipink28 <vipinkalyan28@gmail.com>
2026-08-12 13:27:32 +0530

    user edit popups added.

---
+++ src/helper/index.js
@@ -0,0 +1,5 @@
+export const formatDate = (dateString) => {
+  // convert the date to this format - 12/08/2026 9:00 AM
+
+  return dateString;
+};
--- src/pages/admin/Users.jsx
+++ src/pages/admin/Users.jsx
@@ -1,13 +1,38 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Button from "../../components/form/Button";
 import Popup from "../../components/layout/Popup";
 import UserForm from "../../components/form/UserForm";
 import api from "../../api/config";
 import { Eye, PenSquare, Trash } from "lucide-react";
+import { formatDate } from "../../helper";
+
+// reducer function has two parameters - state, action
+// state - it contains current state value.
+// action - action is an object which has two properties - type and payload. (payload is optional property);
+//const reducer = (state, action)=>{}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "VIEW":
+      return { contentType: "view", data: action.payload };
+    case "EDIT":
+      return { contentType: "edit", data: action.payload };
+    case "DELETE":
+      return { contentType: "delete", data: action.payload };
+    default:
+      return state;
+  }
+};
 
 const Users = () => {
   const [showPopup, setShowPopup] = useState(false);
+  const [isUserPopup, setIsUserPopup] = useState(false);
   const [usersList, setUsersList] = useState(null);
+
+  const [state, dispatch] = useReducer(reducer, null);
+  // dispatch method is used to dispatch the action to reducer function.
+  // dispatch method will take an object as argument - {type:"", payload:""}
+  //dispatch({type:"value", payload:"value"})
 
   const fetchUsers = async (role = "teacher", isActive = true) => {
     try {
@@ -51,13 +76,31 @@ const Users = () => {
                     {user.isActive ? "Active" : "Inactive"}
                   </div>
                   <div className="w-3/12 p-2">
-                    <button className="px-2">
+                    <button
+                      onClick={() => {
+                        setIsUserPopup(true);
+                        dispatch({ type: "VIEW", payload: user });
+                      }}
+                      className="px-2"
+                    >
                       <Eye />
                     </button>
-                    <button className="px-2">
+                    <button
+                      className="px-2"
+                      onClick={() => {
+                        setIsUserPopup(true);
+                        dispatch({ type: "EDIT", payload: user });
+                      }}
+                    >
                       <PenSquare />
                     </button>
-                    <button className="px-2">
+                    <button
+                      className="px-2"
+                      onClick={() => {
+                        setIsUserPopup(true);
+                        dispatch({ type: "DELETE", payload: user });
+                      }}
+                    >
                       <Trash />
                     </button>
                   </div>
@@ -72,6 +115,20 @@ const Users = () => {
       {showPopup && (
         <Popup onClose={setShowPopup}>
           <UserForm onClose={setShowPopup} />
+        </Popup>
+      )}
+
+      {isUserPopup && (
+        <Popup onClose={setIsUserPopup}>
+          {state.contentType === "view" ? (
+            <div>
+              <p>{formatDate(state.data.createdAt)}</p>
+            </div>
+          ) : state.contentType === "edit" ? (
+            <div>Edit</div>
+          ) : (
+            <div>Delete</div>
+          )}
         </Popup>
       )}
     </div>
